f112d5170216adf38e043d6b7cefeaf9c635381c
Koushik Saha <koushik.saha666@gmail.com>
2025-08-04 00:03:31 -0700

    fixed style issue

--- frontend/src/components/auth/AuthModal.tsx
+++ frontend/src/components/auth/AuthModal.tsx
@@ -36,17 +36,8 @@ export default function AuthModal({ isOpen, onClose, initialMode = 'login' }: Au
           className="absolute -top-4 -right-4 bg-white rounded-full p-2 shadow-lg hover:bg-gray-100 transition-colors z-10"
           aria-label="Close"
         >
-          <svg 
-            className="w-5 h-5 text-gray-600" 
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path 
-              strokeLinecap="round" 
-              strokeLinejoin="round" 
-              strokeWidth={2} 
-              d="M6 18L18 6M6 6l12 12" 
-            />
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 20L4 4.00003M20 4L4.00002 20" stroke="black" strokeWidth="2" strokeLinecap="round"/>
           </svg>
         </button>
         
